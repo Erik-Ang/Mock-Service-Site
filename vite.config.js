f75c7fbc26a4918e4a9ab2a1642c0b759a7421c6
Erik Ang <137373695+Erik-Ang@users.noreply.github.com>
2025-06-08 14:45:17 -0400

    Test

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/mock-service-site/',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
 })
